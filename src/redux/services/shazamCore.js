d7ce841a2a8bac1389942e77d916d3048dc7680f
purpleSage <hectormza21@gmail.com>
2023-02-01 12:36:54 -0400

    ...

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -19,7 +19,7 @@ export const shazamCoreApi = createApi({
 			query: (genre) => `v1/charts/genre-world?genre_code=${genre}`,
 		}),
 		getSongDetails: builder.query({
-			query: ({ songid }) => `v1/tracks/details?track_id=${songid}`,
+			query: ({ songid }) => `v2/tracks/details?track_id=${songid}`,
 		}),
 		getSongRelated: builder.query({
 			query: ({ songid }) => `v1/tracks/related?track_id=${songid}`,
